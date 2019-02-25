d8b800bdc3f3918f3425c2a1512910897740115c
Gabi <gabriel.fresco@yahoo.com.ar>
2019-02-25 20:36:45 -0300

    Started project

---
+++ src/main/java/com/gfresco/despegar/DespegarApplication.java
@@ -0,0 +1,13 @@
+package com.gfresco.despegar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespegarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespegarApplication.class, args);
+	}
+
+}
